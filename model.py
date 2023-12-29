861b213c790560874747aa90c3b5cf3258a6dbbf
georgescutelnicu <george.v.scutelnicu@gmail.com>
2023-12-29 22:46:46 +0200

    Add default values to the Destination db

--- model.py
+++ model.py
@@ -13,29 +13,31 @@ class Destination(db.Model):
     country = db.Column(db.String(250), nullable=False)
     city = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250), nullable=False)
-    budget = db.Column(db.String(10), CheckConstraint("budget IN ('affordable', 'moderate', 'expensive')"))
+    budget = db.Column(db.String(10), CheckConstraint("budget IN ('affordable', 'moderate', 'expensive')"),
+                       default='moderate')
 
-    eating_out = db.Column(db.Integer, CheckConstraint('eating_out >= 0 AND eating_out <= 5'))
-    sightseeing = db.Column(db.Integer, CheckConstraint('sightseeing >= 0 AND sightseeing <= 5'))
-    activities = db.Column(db.Integer, CheckConstraint('activities >= 0 AND activities <= 5'))
-    shopping = db.Column(db.Integer, CheckConstraint('shopping >= 0 AND shopping <= 5'))
-    nightlife = db.Column(db.Integer, CheckConstraint('nightlife >= 0 AND nightlife <= 5'))
-    museums = db.Column(db.Integer, CheckConstraint('museums >= 0 AND museums <= 5'))
-    kid_friendly = db.Column(db.Integer, CheckConstraint('kid_friendly >= 0 AND kid_friendly <= 5'))
+    eating_out = db.Column(db.Integer, CheckConstraint('eating_out >= 0 AND eating_out <= 5'), default=0)
+    sightseeing = db.Column(db.Integer, CheckConstraint('sightseeing >= 0 AND sightseeing <= 5'), default=0)
+    activities = db.Column(db.Integer, CheckConstraint('activities >= 0 AND activities <= 5'), default=0)
+    shopping = db.Column(db.Integer, CheckConstraint('shopping >= 0 AND shopping <= 5'), default=0)
+    nightlife = db.Column(db.Integer, CheckConstraint('nightlife >= 0 AND nightlife <= 5'), default=0)
+    museums = db.Column(db.Integer, CheckConstraint('museums >= 0 AND museums <= 5'), default=0)
+    kid_friendly = db.Column(db.Integer, CheckConstraint('kid_friendly >= 0 AND kid_friendly <= 5'), default=0)
 
-    beaches = db.Column(db.Integer, CheckConstraint('beaches >= 0 AND beaches <= 5'))
-    skiing = db.Column(db.Integer, CheckConstraint('skiing >= 0 AND skiing <= 5'))
-    diving = db.Column(db.Integer, CheckConstraint('diving >= 0 AND diving <= 5'))
-    camping = db.Column(db.Integer, CheckConstraint('camping >= 0 AND camping <= 5'))
-    hiking = db.Column(db.Integer, CheckConstraint('hiking >= 0 AND hiking <= 5'))
-    cycling = db.Column(db.Integer, CheckConstraint('cycling >= 0 AND cycling <= 5'))
-    sailing = db.Column(db.Integer, CheckConstraint('sailing >= 0 AND sailing <= 5'))
+    beaches = db.Column(db.Integer, CheckConstraint('beaches >= 0 AND beaches <= 5'), default=0)
+    skiing = db.Column(db.Integer, CheckConstraint('skiing >= 0 AND skiing <= 5'), default=0)
+    diving = db.Column(db.Integer, CheckConstraint('diving >= 0 AND diving <= 5'), default=0)
+    camping = db.Column(db.Integer, CheckConstraint('camping >= 0 AND camping <= 5'), default=0)
+    hiking = db.Column(db.Integer, CheckConstraint('hiking >= 0 AND hiking <= 5'), default=0)
+    cycling = db.Column(db.Integer, CheckConstraint('cycling >= 0 AND cycling <= 5'), default=0)
+    sailing = db.Column(db.Integer, CheckConstraint('sailing >= 0 AND sailing <= 5'), default=0)
 
-    romantic = db.Column(db.Integer, CheckConstraint('romantic >= 0 AND romantic <= 5'))
-    photography = db.Column(db.Integer, CheckConstraint('photography >= 0 AND photography <= 5'))
+    romantic = db.Column(db.Integer, CheckConstraint('romantic >= 0 AND romantic <= 5'), default=0)
+    photography = db.Column(db.Integer, CheckConstraint('photography >= 0 AND photography <= 5'), default=0)
 
-    popular_attractions = db.Column(db.String(500), nullable=True)
-    picture = db.Column(db.String(500), nullable=True)
+    popular_attractions = db.Column(db.String(500), nullable=False)
+    picture = db.Column(db.String(500),
+                        default='https://images.unsplash.com/photo-1506012787146-f92b2d7d6d96?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
 
     def to_dict(self):
         data = {'id': getattr(self, "id"),
