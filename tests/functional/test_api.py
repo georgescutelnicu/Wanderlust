072788512ce72f3712ca0200dc5c34fc854e634a
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-02-05 03:07:52 +0200

    Add functional test for API endpoints

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -64,3 +64,89 @@ def test_destination_weather(client):
 
     assert response_404.status_code == 404
     assert "City not found" in response_404.json["error"]
+
+
+def test_add_destination(client):
+    data = {
+        "city": "Bucharest",
+        "continent": "Europe",
+        "country": "Unknown",
+        "description": "A beautiful destination",
+        "popular_attractions": "Attraction A, Attraction B",
+    }
+
+    response_400 = client.post(f"/api/add_destination", json=data, headers=headers)
+
+    assert response_400.status_code == 400
+    assert f"{data['city']} is already in our database" in response_400.json["error"]
+
+    data["city"] = 123
+
+    response_500 = client.post(f"/api/add_destination", json=data, headers=headers)
+
+    assert response_500.status_code == 500
+    assert "An error has occurred!" in response_500.json["error"]
+
+    db.session.rollback()
+    data["city"] = "Arad42"
+
+    response_400 = client.post(f"/api/add_destination", json=data, headers=headers)
+
+    assert response_400.status_code == 400
+    assert f"{data['country']} is not a valid country name" in response_400.json["error"]
+
+    data["country"] = "Romania"
+
+    response_201 = client.post(f"/api/add_destination", json=data, headers=headers)
+
+    assert response_201.status_code == 201
+    assert "Destination added successfully" in response_201.json["message"]
+
+
+def test_update_destination(client):
+    destination_id = Destination.query.order_by(Destination.id.desc()).first().id
+
+    data = {
+        "country": "XYZ",
+    }
+
+    response_400 = client.patch(f'/api/update_destination/{destination_id}', json=data, headers=headers)
+
+    assert response_400.status_code == 400
+    assert "not a valid country name" in response_400.json["error"]
+
+    data = {
+        "continent": "XYZ"
+    }
+
+    response_400 = client.patch(f'/api/update_destination/{destination_id}', json=data, headers=headers)
+
+    assert response_400.status_code == 400
+    assert "Invalid continent" in response_400.json["error"]
+
+    data = {
+        "popular_attractions": "Attraction X, Attraction Y, Attraction Z",
+        "hiking": 5,
+        "budget": "affordable"
+    }
+
+    response_200 = client.patch(f'/api/update_destination/{destination_id}', json=data, headers=headers)
+
+    assert response_200.status_code == 200
+    assert f"Destination with ID {destination_id} updated successfully" in response_200.json["message"]
+
+
+def test_delete_destination(client):
+    destination_id = Destination.query.order_by(Destination.id.desc()).first().id + 1
+
+    response_404 = client.delete(f"/api/delete_destination/{destination_id}", headers=headers)
+
+    assert response_404.status_code == 404
+    assert f"Destination with ID {destination_id} was not found" in response_404.json["error"]
+
+    destination_id -= 1
+
+    response_200 = client.delete(f"/api/delete_destination/{destination_id}", headers=headers)
+
+    assert response_200.status_code == 200
+    assert f"Destination with ID {destination_id} has been deleted" in response_200.json["message"]
